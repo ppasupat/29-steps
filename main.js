25ba0d026c464e727bcf690a5830a5cc2202ee6e
Ice Pasupat <panuponggong@gmail.com>
2022-01-23 19:26:45 -0800

    Map!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function () {
   'use strict';
 
-  const SCREEN_WIDTH = 700, SCREEN_HEIGHT = 500;
+  const SCREEN_WIDTH = 700, SCREEN_HEIGHT = 400;
   const FRAME_RATE = 8;
 
   // ################################
@@ -19,8 +19,19 @@ $(function () {
   // ################################
   // Main game UI
 
-  function setupMenu() {
+  const MAP_ROW_HEIGHT = 320, MAP_COL_WIDTH = 200,
+    MAP_TOP_MARGIN = 10, MAP_LEFT_MARGIN = 0;
+
+  function moveMap(row, col) {
+    $('#map').css({
+      top: -(MAP_TOP_MARGIN + MAP_ROW_HEIGHT * row) + 'px',
+      left: -(MAP_LEFT_MARGIN + MAP_COL_WIDTH * col) + 'px',
+    });
+  }
+
+  function setupMain() {
     showScene('main');
+    moveMap(5, 0);
   }
 
   // ################################
@@ -44,13 +55,14 @@ $(function () {
 
   const imageList = [
     'img/heart.png',
+    'img/map-draft.png',
   ];
   let numResourcesLeft = imageList.length;
 
   function decrementPreload () {
     numResourcesLeft--;
     if (numResourcesLeft === 0) {
-      setupMenu();
+      setupMain();
     } else {
       $('#pane-loading').text('Loading resources (' + numResourcesLeft + ' left)');
     }
